2b3e8681dea809a85dc6b2e5fd794afb5d0cacc4
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-01 20:33:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
-
-
-name = ["Ada", "Grace", "Kent"]
+katz_deli = []
+def ake_a_number(name, katz_deli)
+  if name = ["Ada", "Grace", "Kent"]
   puts "Welcome, #{name}. You are number #{name.length}."
   end 
 end
