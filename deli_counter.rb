2abf76184dbd830752def1005df4f22e9047e71d
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-01 21:36:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ name = ("Ada", "Grace", "Kent")
 katz_deli = (0, 1, 2, 3)
 
 def deli_counter (name, katz_deli)
-  if katz_deli = 0 
+  when katz_deli = 0 
     puts "The line is currently empty"
   elsif katz_deli.shift > 0 
   return "#{name}, #{katz_deli}"
