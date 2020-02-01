ecd94539ce27d6ae07ebcf2092074f5237665b8b
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-01 21:46:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ katz_deli = []
 def deli_counter (katz_deli)
   if katz_deli = []
     puts "The line is currently empty"
-  elsif take_number > 0 
+  while take_number > 0 
     puts "#{name}, #{katz_deli}"
     end 
 end 
