f6f0265e88c49718e70e08e4966ac54c3d476423
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-01 21:29:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,7 @@ katz_deli = (0, 1, 2, 3)
 def deli_counter (name, katz_deli)
   if katz_deli = 0 
     puts "The line is currently empty"
-  elsif katz_deli.shift = num 
-     num.each do |num|
-    puts ""
+  elsif katz_deli.shift > 0 
+  puts "#{name}, #{katz_deli}"
   
     
