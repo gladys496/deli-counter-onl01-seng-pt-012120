ad80181ff06393a49e7d2635639f62a567f7911e
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-01 22:13:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,12 @@
 katz_deli = []
-
+other_deli = ["1. Logan 2. Avi 3.spencer" ]
 
 
 def line (katz_deli, other_deli)
   if katz_deli = []
     puts "The line is currently empty."
-  else other_deli
+  else 
+    puts "The line is currently: #{other_deli}"
     
     end 
 end 
