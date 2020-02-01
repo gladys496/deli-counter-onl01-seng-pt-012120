dbeb851e50b00727e1a9224c4824fd396b61b0de
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-01 21:47:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,9 @@ take_a_number = (1, 2, 3)
 katz_deli = []
 
 def deli_counter (katz_deli)
-  if katz_deli = []
+  while katz_deli = []
     puts "The line is currently empty"
-  while take_number > 0 
+  until take_number > 0 
     puts "#{name}, #{katz_deli}"
     end 
 end 
