766e274402463706081161f61e6b26f2ad5edb02
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-01 21:51:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ name = ("Ada", "Grace", "Kent")
 take_a_number = (1, 2, 3)
 katz_deli = []
 
-def deli_counter (katz_deli,take_a_number,name)
+def deli_counter (take_a_number,name,katz_deli)
   if katz_deli = []
     puts "The line is currently empty"
   elsif take_number > 0 
