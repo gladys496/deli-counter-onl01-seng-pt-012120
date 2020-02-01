a16c47e69b7c5e3abf3d34f429b9d9cf903d098f
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-01 21:38:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,11 @@ name = ("Ada", "Grace", "Kent")
 take_a_number = (0, 1, 2, 3)
 katz_deli = []
 
-
+elsif katz_deli.shift > 0 
+  return "#{name}, #{katz_deli}"
+    end 
+end 
+  
 
 def deli_counter (katz_deli)
   when katz_deli = []
